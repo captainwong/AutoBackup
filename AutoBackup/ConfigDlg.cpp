4c2714530adae163f5c8f93940ab8f4ed539b4b8
i7-8700 <1281261856@qq.com>
2022-06-08 00:47:32 +0800

    update

--- AutoBackup/ConfigDlg.cpp
+++ AutoBackup/ConfigDlg.cpp
@@ -46,40 +46,40 @@ END_MESSAGE_MAP()
 /////////////////////////////////////////////////////////////////////////////
 // CConfigDlg message handlers
 
-void CConfigDlg::OnOK() 
+void CConfigDlg::OnOK()
 {
 	// TODO: Add extra validation here
-	
+
 	CDialog::OnOK();
 }
 
-void CConfigDlg::OnCancel() 
+void CConfigDlg::OnCancel()
 {
 	// TODO: Add extra cleanup here
-	
+
 	CDialog::OnCancel();
 }
 
-BOOL CConfigDlg::OnInitDialog() 
+BOOL CConfigDlg::OnInitDialog()
 {
 	CDialog::OnInitDialog();
-	
+
 	// TODO: Add extra initialization here
 	InitCfg();
 	return TRUE;  // return TRUE unless you set the focus to a control
-	              // EXCEPTION: OCX Property Pages should return FALSE
+				  // EXCEPTION: OCX Property Pages should return FALSE
 }
 
 void CConfigDlg::InitCfg()
 {
-	CAutoBackupApp *afxApp = (CAutoBackupApp*)AfxGetApp();
+	CAutoBackupApp* afxApp = (CAutoBackupApp*)AfxGetApp();
 	ASSERT(afxApp);
 	HKEY hKey = NULL;
 	LONG lRet = ::RegOpenKey(HKEY_CURRENT_USER, _T("SoftWare\\AutoBackup\\AutoBackup\\Exts"), &hKey);
-	if(lRet != ERROR_SUCCESS)	//子键不存在，创建之，创建多个键值
+	if (lRet != ERROR_SUCCESS)	//子键不存在，创建之，创建多个键值
 	{
 		lRet = ::RegCreateKey(HKEY_CURRENT_USER, _T("SoftWare\\AutoBackup\\AutoBackup\\Exts"), &hKey);
-		if(lRet != ERROR_SUCCESS)
+		if (lRet != ERROR_SUCCESS)
 			return;
 
 		// 初始化 后缀名列表
@@ -115,38 +115,31 @@ void CConfigDlg::InitCfg()
 		afxApp->WriteProfileInt(_T("Exts"), _T("SBR"), 1);
 
 		POSITION pos = afxApp->m_extList.GetHeadPosition();
-		while(pos)
-		{
+		while (pos) {
 			m_list1.AddString(afxApp->m_extList.GetNext(pos));
 		}
-	}
-	else		// 子键存在，读取所有键值
+	} else		// 子键存在，读取所有键值
 	{
 		DWORD dwSubValues, dwMaxValueNameLen, dwMaxValueLen;
-		lRet = ::RegQueryInfoKey(hKey, NULL, NULL, NULL, 
+		lRet = ::RegQueryInfoKey(hKey, NULL, NULL, NULL,
 			NULL, NULL, NULL, &dwSubValues, &dwMaxValueNameLen, &dwMaxValueLen, NULL, NULL);
-		if(lRet == ERROR_SUCCESS)
-		{
+		if (lRet == ERROR_SUCCESS) {
 			DWORD cMaxValueNameLen = dwMaxValueNameLen + 1;
 			DWORD dwType = REG_DWORD;
-			for(DWORD i = 0; lRet == ERROR_SUCCESS, i < dwSubValues; i++)
-			{
+			for (DWORD i = 0; lRet == ERROR_SUCCESS && i < dwSubValues; i++) {
 				dwMaxValueNameLen = cMaxValueNameLen;	// for nul
 				unsigned char* lpData = new unsigned char[dwMaxValueLen];
-				TCHAR *szSubValueName = new TCHAR[cMaxValueNameLen];
+				TCHAR* szSubValueName = new TCHAR[cMaxValueNameLen];
 				memset(szSubValueName, 0, sizeof(szSubValueName));
-				lRet = RegEnumValue(hKey, i, szSubValueName, &dwMaxValueNameLen, NULL, &dwType, lpData, &dwMaxValueLen); 
+				lRet = RegEnumValue(hKey, i, szSubValueName, &dwMaxValueNameLen, NULL, &dwType, lpData, &dwMaxValueLen);
 				CString str = _T("");
 				str.Format(_T("%s %d\n"), szSubValueName, *lpData);
 				TRACE(str);
 				int value = *(int*)lpData;
-				if(value == 1)
-				{
+				if (value == 1) {
 					afxApp->m_extList.AddTail(CString(szSubValueName));
 					m_list2.AddString(szSubValueName);
-				}
-				else
-				{
+				} else {
 					m_list1.AddString(szSubValueName);
 				}
 				SAFEDELETEARR(lpData);
@@ -156,58 +149,53 @@ void CConfigDlg::InitCfg()
 	}
 }
 
-void CConfigDlg::OnButtonMovl() 
+void CConfigDlg::OnButtonMovl()
 {
 	int index = m_list2.GetCurSel();
-	if(index < 0)	return;
+	if (index < 0)	return;
 
 	CString ext = _T("");
 	m_list2.GetText(index, ext);
 
-	do
-	{
-		if(LB_ERR == m_list2.DeleteString(index))
+	do {
+		if (LB_ERR == m_list2.DeleteString(index))
 			break;
-		if(LB_ERR == m_list1.AddString(ext))
+		if (LB_ERR == m_list1.AddString(ext))
 			break;
-		if(!SetKeyValue(ext, 0))
+		if (!SetKeyValue(ext, 0))
 			break;
-	
+
 		m_list2.SetCurSel(index);
-	}
-	while(0);
+	} while (0);
 }
 
-void CConfigDlg::OnButtonMovr() 
+void CConfigDlg::OnButtonMovr()
 {
 	int index = m_list1.GetCurSel();
-	if(index < 0)	return;
+	if (index < 0)	return;
 
 	CString ext = _T("");
 	m_list1.GetText(index, ext);
 
-	do
-	{
-		if(LB_ERR == m_list1.DeleteString(index))
+	do {
+		if (LB_ERR == m_list1.DeleteString(index))
 			break;
-		if(LB_ERR == m_list2.AddString(ext))
+		if (LB_ERR == m_list2.AddString(ext))
 			break;
-		if(!SetKeyValue(ext, 1))
+		if (!SetKeyValue(ext, 1))
 			break;
 
 		m_list1.SetCurSel(index);
-	}
-	while(0);
+	} while (0);
 }
 
 BOOL CConfigDlg::SetKeyValue(LPCTSTR lpszValueName, DWORD dwValueData)
 {
-	CAutoBackupApp *afxApp = (CAutoBackupApp*)AfxGetApp();
+	CAutoBackupApp* afxApp = (CAutoBackupApp*)AfxGetApp();
 	ASSERT(afxApp);
 
-	if(afxApp->WriteProfileInt(_T("Exts"), lpszValueName, dwValueData))
-	{
-		if(dwValueData == 0)
+	if (afxApp->WriteProfileInt(_T("Exts"), lpszValueName, dwValueData)) {
+		if (dwValueData == 0)
 			afxApp->m_extList.RemoveAt(afxApp->m_extList.Find(lpszValueName));
 		else
 			afxApp->m_extList.AddTail(lpszValueName);
@@ -218,20 +206,17 @@ BOOL CConfigDlg::SetKeyValue(LPCTSTR lpszValueName, DWORD dwValueData)
 
 
 
-void CConfigDlg::OnButtonAdd() 
+void CConfigDlg::OnButtonAdd()
 {
 	CInputExtDlg ie;
-	if(IDOK == ie.DoModal())
-	{
+	if (IDOK == ie.DoModal()) {
 		CString ext = ie.m_ext;
 		ext.MakeUpper();
-		do
-		{
-			if(LB_ERR == m_list2.AddString(ext))
+		do {
+			if (LB_ERR == m_list2.AddString(ext))
 				break;
-			if(!SetKeyValue(ext, 1))
+			if (!SetKeyValue(ext, 1))
 				break;
-		}
-		while(0);
+		} while (0);
 	}
 }
